68dddae9b7fb6e747f891af992d44cc8b0d594da
Jessica Contreras Ayala <hoshostars@hotmail.com>
2024-02-19 14:07:38 -0500

    .

---
+++ notes.js
@@ -0,0 +1,9 @@
+
+// run  npm init -y
+// dl jest: npm --save-dev install jest
+// on package.json file. make sure on test: it says "test": "jest" 
+
+//create file backend.test.js
+// import app.js file 
+
+// change git remote : git remote set-url origin ""
